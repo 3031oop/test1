ddd03bc5d3218bc65b42d59bb0fe72b7503e9958
3031oop <user.emial>
2025-10-28 16:53:23 +0900

    first commit

---
+++ day9/day9_2.c
@@ -0,0 +1,32 @@
+// 17-2 배열과 포인터를 멤버로 갖는 구조체 사용
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+struct profile{
+    char name[20];
+    int age;
+    double height;
+    char *intro;
+};
+
+int main(void){
+    struct profile yuni;
+    strcpy(yuni.name, "서하윤");
+    yuni.age = 17;
+    yuni.height = 164.5;
+    
+    yuni.intro = (char *)malloc(80);
+    printf("자기 소개 : ");
+    gets(yuni.intro);
+
+    printf("이름 : %s\n", yuni.name);
+    printf("나이 : %d\n", yuni.age);
+    printf("키 : %.1lf\n", yuni.height);
+    printf("자기소개 : %s\n", yuni.intro);
+
+    return 0;
+
+
+}
---
+++ day9/day9_3.c
@@ -0,0 +1,36 @@
+// 17-3 다른 구조체의 멤버로 다른 구조체 사용하기 
+#include <stdio.h>
+
+struct address {
+    char name[20];
+    int age;
+    char tel[20];
+    char addr[80];
+};
+
+void print_list(struct address *lp);
+
+int main(void){
+    struct address list[5] = {
+        {"홍길동", 23, "111-1111", "울릉도 독도"},
+        {"이순신", 35, "222-2222", "서울 건천동"},
+        {"장보고", 19, "333-3333", "완동 청해진"},
+        {"유관순", 15, "444-4444", "충남 천안 "},
+        {"안중근", 45, "555-5555", "황해도 해주"}
+    };
+
+    print_list(list);
+
+    
+    return 0;
+}
+
+void print_list(struct address *lp){
+    int i;
+
+    for (i=0; i<5; i++){
+        printf("%10s%5d%15s%20s\n",
+            (lp+i)->name, (lp+i)->age,(lp+i)->tel,(lp+i)->addr);
+    }
+
+}
